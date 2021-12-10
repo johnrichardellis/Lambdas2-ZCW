a230ad351c2e3d6c2e474a0e0970a2e8b127fff1
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-12-09 20:11:42 -0500

    lab

--- src/main/java/Anonymous.java
+++ src/main/java/Anonymous.java
@@ -1,6 +1,20 @@
+import java.util.List;
+
 public class Anonymous implements CheckPerson{
     @Override
     public boolean test(Person p) {
         return false;
+    }
+
+
+
+
+    public static void printPersons(
+            List<Person> roster, CheckPerson tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                p.printPerson();
+            }
+        }
     }
 }
--- src/main/java/Local.java
+++ src/main/java/Local.java
@@ -1,6 +1,25 @@
+import java.util.List;
+
 public class Local implements CheckPerson{
     @Override
     public boolean test(Person p) {
+        if (p.name.equals("Frank") && p.age == 34) {
+            return true;
+        }
         return false;
+    }
+
+
+
+
+
+
+    public static void printPersons(
+            List<Person> roster, CheckPerson tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                p.printPerson();
+            }
+        }
     }
 }
